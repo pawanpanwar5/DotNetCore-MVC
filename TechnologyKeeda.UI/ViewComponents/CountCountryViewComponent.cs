bd39952cf9869270e99c58a1118512f3719cc453
pawanpanwar5 <pawanp.panwar@gmail.com>
2024-09-07 16:35:03 +0530

    partial Views created and view components added

---
+++ TechnologyKeeda.UI/ViewComponents/CountCountryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnologyKeeda.UI.ViewComponents
+{
+    public class CountCountryViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TechnologyKeeda.UI/ViewComponents/CountStateViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnologyKeeda.UI.ViewComponents
+{
+    public class CountStateViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
