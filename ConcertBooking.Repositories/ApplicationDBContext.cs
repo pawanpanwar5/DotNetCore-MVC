4815154be0bb1bc973a6b0b46d80736930c3f3d1
pawanpanwar5 <pawanp.panwar@gmail.com>
2024-09-11 13:39:40 +0530

    added Entities and Repo for concert booking system

---
+++ ConcertBooking.Repositories/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using ConcertBooking.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnologyKeeda.ConcertBooking.Repositories
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        
+        }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Concert> Concerts { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        
+
+    }
+}
